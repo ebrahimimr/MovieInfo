079700ab93689cb155f1aa7388940aafd1b682f8
Ebrahimi, Mohammad Reza (Software & Information Systems) <ebrahimi_mr@saipacorp.com>
2021-11-30 13:52:12 +0330

    first commit

--- app/src/main/java/ir/mreonline/movieinfo/MainActivity.java
+++ app/src/main/java/ir/mreonline/movieinfo/MainActivity.java
@@ -1,11 +1,17 @@
 package ir.mreonline.movieinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+//git remote add origin https://github.com/ebrahimimr/MovieInfo.git
+//git branch -M main
+//git push -u origin main
+public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
